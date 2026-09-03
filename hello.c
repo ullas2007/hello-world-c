c53915d674fc9bb5a11f073f323c1fcd571bf60d
Ullas M J <ugcet2502460@reva.edu.in>
2026-09-03 13:11:10 +0530

    Add greet() function — paired with <Sai Sujay P> via Live Share

--- hello.c
+++ hello.c
@@ -2,5 +2,9 @@
 
 int main() {
     printf("Hello from c!\n");
+    greet("ada");
     return 0;
+}
+void greet(const char *name) {
+ printf("Hello, %s! Welcome to your GitHub portfolio.\n", name);
 }
